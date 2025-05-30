38bc6ff08a382ed0f3aa02066a8f8faa453dcb40
Shobhit-DataScientist <shobhit@globussoft.in>
2025-05-30 11:54:58 +0530

    edit

--- fetch_dataset.py
+++ fetch_dataset.py
@@ -5,4 +5,11 @@ rf = Roboflow(api_key="SkpACeECQkUHe2S3K5rw")
 project = rf.workspace("mamytest").project("test_smoke_fire_model")
 version = project.version(1)
 dataset = version.download("yolov8")
+
+
+
+
+## raw url - https://app.roboflow.com/ds/rX8FkZtKWa?key=vlvAj87KmG
+
+# cmd  curl -L "https://app.roboflow.com/ds/rX8FkZtKWa?key=vlvAj87KmG" > roboflow.zip; unzip roboflow.zip; rm roboflow.zip
                 
